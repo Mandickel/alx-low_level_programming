377bee3cd721b27d8a9c47523f1fb8873f638792
Mandickel <mandickel@gmail.com>
2022-12-12 07:59:07 -0800

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - starting point
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	char c;
+	
+	for (c='a'; c <= 'z'; ++c)
+	     putchar(c);
+	     return (0);
+}
