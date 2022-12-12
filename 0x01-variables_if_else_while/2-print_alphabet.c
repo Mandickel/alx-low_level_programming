74c750ef01e6ab51ccc3baaf188fd92f14cf4464
Mandickel <mandickel@gmail.com>
2022-12-12 09:05:36 -0800

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,17 @@
  *
  * Return: ::0
  **/
-	int main(void) {
+	int main(void) 
+	{
 
 	char c = 'a';
 
-	while (c <= 'z') {
+	while (c <= 'z') 
+	{
 	     putchar(c);
 	     c++;
 	}
 	putchar('\n');
 
 	return (0);
-}
+	}
