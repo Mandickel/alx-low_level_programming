22a74aeecca731411e514f973cb8b1f289bf8842
Mandickel <mandickel@gmail.com>
2022-12-13 09:04:22 -0800

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,7 @@
  * Description - function that prints the alphabet, in lowercase, followed by a new line.
  * Return: 0
  **/
-
-void print_alphabet() {
+void print_alphabet(void) {
 	  char letter;
 
 	    for (letter = 'a'; letter <= 'z'; letter++) {
