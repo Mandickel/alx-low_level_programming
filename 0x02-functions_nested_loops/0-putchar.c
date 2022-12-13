ccec4e70bfb9786194710fe9e04ffe9c9bfbfc41
Mandickel <mandickel@gmail.com>
2022-12-12 23:35:12 -0800

    A program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Staring point
+ * Description - A program that prints _putchar, followed by a new line.
+ * Return: 0
+ **/
+int main()
+{
+	printf ("_putchar\n");
+	return 0;
+}
