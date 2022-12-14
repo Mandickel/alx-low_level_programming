1b32192e45ad85cdb30a3dc3369eb190bb62fb4b
Mandickel <mandickel@gmail.com>
2022-12-13 21:39:43 -0800

    a function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,22 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		printf('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		printf('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		printf('-');
 		return (-1);
 	}
 	else
 	{
-		printf("-");
-		return ("/");
+		printf('-');
+		return ('/');
 	}
 }
