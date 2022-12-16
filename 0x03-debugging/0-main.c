ef320d5dcbcaca24611fe3e8223362b0c94b94d1
Mandickel <mandickel@gmail.com>
2022-12-16 03:10:46 -0800

    testing code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,15 @@
 #include "main.h"
 /**
- * tester - Checking it function is working
+ * main - Checking it function is working
  * Return: 0
  *
  **/
-int tester(void)
+int main(void)
 {
 	int i;
 
-	i = 98;
-	if(i > 0)
-	{
-		positive_or_negative(i);
-		return (0);
-	}
-	else
-	{
-		printf("%d is zero",i);
-		return (0);
-	}
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
