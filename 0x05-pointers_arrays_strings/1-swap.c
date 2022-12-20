bc711426c12e92ced53bf6b76cc294d7e75c04e3
Mandickel <mandickel@gmail.com>
2022-12-19 20:29:41 -0800

    Writting a function that swaps the values of two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swapping values of different variables
+ * @i: variable 1
+ * @n: variable 2
+ * Return: void
+ **/
+void swap_int(int *i, int *n)
+{
+	int tmp;
+	tmp = *i;
+	*i = *n;
+	*n = tmp;
+}
