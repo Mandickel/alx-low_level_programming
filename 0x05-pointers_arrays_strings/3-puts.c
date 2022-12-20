04d5fab3c57da8ec19caf8f381798673d1cbd820
Mandickel <mandickel@gmail.com>
2022-12-19 21:00:36 -0800

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string
+ * @str: Variable
+ * Return: 0
+ **/
+void _puts(char *str)
+{
+	int i;
+
+	i = *str;
+
+	puts(i); //writting the string to stdout
+}
