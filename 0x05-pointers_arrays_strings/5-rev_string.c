f810247224ffeb958153b7badb92bbd7645c2281
Mandickel <mandickel@gmail.com>
2022-12-20 01:32:02 -0800

    function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s:variable
+ * Return: 0
+ * */
+void rev_string(char *s)
+{
+	int i,j,temp,length;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	length = i;
+	i = 0;
+	j = length - 1;
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
