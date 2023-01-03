df1a6c6dedbd2c7c3c9c2ab76739dd35db7766d6
Mandickel <mandickel@gmail.com>
2023-01-02 17:01:19 -0800

    a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: char
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j;
+
+	while (i < 8)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
